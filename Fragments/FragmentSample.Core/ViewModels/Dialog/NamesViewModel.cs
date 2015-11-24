6f55c2beaf0fce416525e123e796dbd166512507
Flavius Demian <flaviusdemian91@yahoo.com>
2015-11-24 22:04:29 +0200

    working copy on command

--- Fragments/FragmentSample.Core/ViewModels/Dialog/NamesViewModel.cs
+++ Fragments/FragmentSample.Core/ViewModels/Dialog/NamesViewModel.cs
@@ -1,9 +1,13 @@
-﻿namespace FragmentSample.Core.ViewModels.Dialog
+﻿using System.Windows.Input;
+using Cirrious.MvvmCross.ViewModels;
+
+namespace FragmentSample.Core.ViewModels.Dialog
 {
     public class NamesViewModel : BaseViewModel
     {
         private string _firstName;
         private string _lastName;
+        private ICommand mCloseCommand;
 
         public NamesViewModel()
         {
@@ -37,5 +41,21 @@
         {
             get { return string.Format(@"{0} {1}", _firstName, _lastName); }
         }
+
+        public ICommand CloseCommand
+        {
+            get
+            {
+                mCloseCommand = mCloseCommand ?? new MvxCommand(CloseDialog);
+                return mCloseCommand;
+            }
+        }
+
+        private void CloseDialog()
+        {
+            int x = 9;
+            x++;
+        }
+
     }
 }
